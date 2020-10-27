acffef34eabc3a745ecef7f1498bfe615446f5a5
jacob <jsylvest@hotmail.dk>
2020-10-27 13:02:23 +0100

    Initialiseringen af et nyt spil, samt opdatering af nyt ord, gæt bogstav mm.

--- app/src/main/java/com/example/galgeleg_min/Galgelogik.java
+++ app/src/main/java/com/example/galgeleg_min/Galgelogik.java
@@ -9,7 +9,7 @@ import java.util.HashSet;
 import java.util.Random;
 
 public class Galgelogik {
-    /** AHT afprøvning er muligeOrd synlig på pakkeniveau */
+
     ArrayList<String> muligeOrd = new ArrayList<String>();
     private String ordet;
     private ArrayList<String> brugteBogstaver = new ArrayList<String>();
@@ -29,8 +29,124 @@ public class Galgelogik {
         muligeOrd.add("princippet");
         muligeOrd.add("syndrom");
         muligeOrd.add("had");
-        muligeOrd.add("gavtyv")
+        muligeOrd.add("gavtyv");
 
         startNytSpil();
     }
+
+    public ArrayList<String> getBrugteBogstaver() {
+        return brugteBogstaver;
+    }
+
+    public String getSynligtOrd() {
+        return synligtOrd;
+    }
+
+    public String getOrdet() {
+        return ordet;
+    }
+
+    public int getAntalForkerteBogstaver() {
+        return antalForkerteBogstaver;
+    }
+
+    public boolean erSidsteBogstavKorrekt() {
+        return sidsteBogstavVarKorrekt;
+    }
+
+    public boolean erSpilletVundet() {
+        return spilletErVundet;
+    }
+
+    public boolean erSpilletTabt() {
+        return spilletErTabt;
+    }
+
+    public boolean erSpilletSlut() {
+        return spilletErTabt || spilletErVundet;
+    }
+
+
+
+    public void startNytSpil() {
+        brugteBogstaver.clear();
+        antalForkerteBogstaver = 0;
+        spilletErVundet = false;
+        spilletErTabt = false;
+        if (muligeOrd.isEmpty()) throw new IllegalStateException("Listen over mulige ord er tom!");
+        ordet = muligeOrd.get(new Random().nextInt(muligeOrd.size()));
+        System.out.println("Nyt spil - det skjulte ord er: "+ordet);
+        opdaterSynligtOrd();
+    }
+
+    private void opdaterSynligtOrd() {
+        synligtOrd = "";
+        spilletErVundet = true;
+        for (int n = 0; n < ordet.length(); n++) {
+            String bogstav = ordet.substring(n, n + 1);
+            if (brugteBogstaver.contains(bogstav)) {
+                synligtOrd = synligtOrd + bogstav;
+            } else {
+                synligtOrd = synligtOrd + "*";
+                spilletErVundet = false;
+            }
+        }
+    }
+
+    public void gætBogstav(String bogstav) {
+        //String str;
+        if (bogstav.length() != 1) return;
+
+        //str = "Der gættes på bogstavet: " + bogstav;
+
+        System.out.println("Der gættes på bogstavet: " + bogstav);
+        if (brugteBogstaver.contains(bogstav)) return;
+        if (spilletErVundet || spilletErTabt) return;
+
+        brugteBogstaver.add(bogstav);
+
+        if (ordet.contains(bogstav)) {
+            sidsteBogstavVarKorrekt = true;
+
+            //str += "\nBogstavet var korrekt: " + bogstav;
+
+            System.out.println("Bogstavet var korrekt: " + bogstav);
+        } else {
+            // Vi gættede på et bogstav der ikke var i ordet.
+            sidsteBogstavVarKorrekt = false;
+
+            //str += "\nBogstavet var IKKE korrekt: " + bogstav;
+
+            System.out.println("Bogstavet var IKKE korrekt: " + bogstav);
+            antalForkerteBogstaver = antalForkerteBogstaver + 1;
+            if (antalForkerteBogstaver > 6) {
+                spilletErTabt = true;
+            }
+        }
+        opdaterSynligtOrd();
+    }
+
+    public static String hentUrl(String url) throws IOException {
+        System.out.println("Henter data fra " + url);
+        BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
+        StringBuilder sb = new StringBuilder();
+        String linje = br.readLine();
+        while (linje != null) {
+            sb.append(linje + "\n");
+            linje = br.readLine();
+        }
+        return sb.toString();
+    }
+
+    public void logStatus() {
+        System.out.println("---------- ");
+        System.out.println("- ordet (skult) = " + ordet);
+        System.out.println("- synligtOrd = " + synligtOrd);
+        System.out.println("- forkerteBogstaver = " + antalForkerteBogstaver);
+        System.out.println("- brugeBogstaver = " + brugteBogstaver);
+        if (spilletErTabt) System.out.println("- SPILLET ER TABT");
+        if (spilletErVundet) System.out.println("- SPILLET ER VUNDET");
+        System.out.println("---------- ");
+    }
+
 }
